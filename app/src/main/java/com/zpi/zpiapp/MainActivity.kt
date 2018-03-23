691f68541ad60e541a06e1d6cc484a298eb598d8
patrykheciak <patrykheciak@gmail.com>
2018-03-23 13:24:27 +0100

    dodano pusty fragment interakcje

--- app/src/main/java/com/zpi/zpiapp/MainActivity.kt
+++ app/src/main/java/com/zpi/zpiapp/MainActivity.kt
@@ -2,31 +2,42 @@ package com.zpi.zpiapp
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var interactionsFragment: InteractionsFragment
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigaation_iteractions -> {
-                message.setText(R.string.title_interactions)
+
+                interactionsFragment = InteractionsFragment()
+                replaceFragment(interactionsFragment)
+
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
+    }
+
+    private fun MainActivity.replaceFragment(fragment: Fragment) {
+
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.content, fragment)
+                .commit()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
