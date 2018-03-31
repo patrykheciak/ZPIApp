da3aec36d0b00bfd2ce5a9eae45fabc1698fb9f9
sirmarjan@gmail.com <MARJANmaniek96>
2018-03-31 16:14:17 +0200

    Init commit

---
+++ app/src/main/java/com/zpi/zpiapp/careAssistants/CareAssistantsActivity.kt
@@ -0,0 +1,14 @@
+package com.zpi.zpiapp.careAssistants
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.zpi.zpiapp.R
+
+
+class CareAssistantsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_care_assistants)
+    }
+}
