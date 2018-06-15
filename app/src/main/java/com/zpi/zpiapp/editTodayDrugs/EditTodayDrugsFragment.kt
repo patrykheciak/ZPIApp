342985c394dc1d754c0b67263fe5a7c5228bc1b5
sirmarjan@gmail.com <MARJANmaniek96>
2018-06-15 10:21:29 +0200

    Wczytywanie CallendarRow

--- app/src/main/java/com/zpi/zpiapp/editTodayDrugs/EditTodayDrugsFragment.kt
+++ app/src/main/java/com/zpi/zpiapp/editTodayDrugs/EditTodayDrugsFragment.kt
@@ -2,20 +2,49 @@ package com.zpi.zpiapp.editTodayDrugs
 
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.zpi.zpiapp.R
+import kotlinx.android.synthetic.main.fragment_today_drugs.*
 
 
-class EditTodayDrugsFragment : Fragment() {
+class EditTodayDrugsFragment : Fragment(),EditTodayDrugsContract.View {
+    private lateinit var mPresenter: EditTodayDrugsContract.Presenter
+    private lateinit var mPatientDrugRowAdapter: PatientDrugRowAdapter
 
+    override fun setPresenter(presenter: EditTodayDrugsContract.Presenter) {
+        mPresenter=presenter
+    }
+
+    override fun setPatientDrugsRow(patientDrugRow: List<PatientDrugRow>) {
+        mPatientDrugRowAdapter.load(patientDrugRow)
+    }
+
+    override fun showSnackBarMessage(message: String) {
+        Snackbar.make(todayDrugRV, message, Snackbar.LENGTH_SHORT).show()
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_today_drugs, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mPatientDrugRowAdapter = PatientDrugRowAdapter()
+        todayDrugRV.adapter = mPatientDrugRowAdapter
+        todayDrugRV.layoutManager = LinearLayoutManager(context)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mPresenter.start()
     }
 
 }
