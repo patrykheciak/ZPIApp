b6437ea82931f153facc02f4691394dec84b5c27
sirmarjan@gmail.com <MARJANmaniek96>
2018-06-28 19:07:31 +0200

    Poprawa url rest dla careassistant i physician

--- app/src/main/java/com/zpi/zpiapp/data/CareAssistantsService.kt
+++ app/src/main/java/com/zpi/zpiapp/data/CareAssistantsService.kt
@@ -10,14 +10,20 @@ import retrofit2.http.Path
 
 interface CareAssistantsService{
 
-    @GET("api/patients/careassistants/{userId}")
+    @GET("$route/PatientCareAssistant/{userId}")
     fun patientCareAssistants(@Path("userId") userId: Int): Call<List<CareAssistant>>
 
-    @POST("api/patients/careassistantAdd/{userId}/{login}")
+    @POST("$route/add/{userId}/{login}")
     fun addCareAssistant(@Path("userId") userId: Int,
                          @Path("login") careAssistantLogin: String):Call<ResponseBody>
 
-    @DELETE("api/patients/careassistantRemove/{userId}/{caId}")
+    @DELETE("$route/remove/{userId}/{caId}")
     fun removeCareAssistant(@Path("userId") userId: Int,
                             @Path("caId") careAssistantId:Int):Call<ResponseBody>
+
+    companion object {
+        const val route = "api/PatientCareAssistant"
+    }
 }
+
+
--- app/src/main/java/com/zpi/zpiapp/data/PhysiciansService.kt
+++ app/src/main/java/com/zpi/zpiapp/data/PhysiciansService.kt
@@ -7,17 +7,22 @@ import retrofit2.http.*
 
 interface PhysiciansService {
 
-    @GET("api/patients/physician/{userId}")
+    @GET("$route/physicians/{userId}")
     fun patientPhysicians(@Path("userId") userId: Int): Call<List<Physician>>
 
-    @PUT("api/patients/physician/{userId}/{pwzNumber}")
+    @PUT("$route/add/{userId}/{pwzNumber}")
     fun addPhysician(@Path("userId") userId: Int,
                          @Path("pwzNumber") pwzNumber: String): Call<ResponseBody>
 
-    @PUT("api/patients/physicianRemove/{userId}/{psId}")
+    @PUT("$route/remove/{userId}/{psId}")
     fun removePhysician(@Path("userId") userId: Int,
                             @Path("psId") physicianId:Int): Call<ResponseBody>
 
     @GET( "api/Physicians" )
     fun getAllPhysicians():Call<List<Physician>>
+
+    companion object {
+        const val route="api/PatientPhysician"
+    }
+
 }
